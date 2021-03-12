e7da71de4f0b727562be3564089e31fd96d34c7d
chrislin <chris.lin.tw@hotmail.com>
2021-03-12 16:29:02 +0800

    1100312_C_創建

--- M11XML/MainForm.cs
+++ M11XML/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace M11XML
 {
-    public partial class Form1 : Form
+    public partial class MainForm : BaseForm
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
