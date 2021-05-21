c6b7d7c69886b664c931edecdb57e0b22f31311a
chrislin <chris.lin.tw@hotmail.com>
2021-05-21 10:56:07 +0800

    1100521_C_新增Google專案提供上傳GoogleDrive工作

---
+++ M11Google/BaseForm.cs
@@ -0,0 +1,69 @@
+﻿using System.Windows.Forms;
+using System.Configuration;
+using M10.lib;
+using NLog;
+using System.Net;
+
+namespace M11Google
+{
+    public class BaseForm : Form
+    {
+        private string _ConnectionString;
+        private DALDapper _dbDapper;
+        public string ssql = string.Empty;
+        public Logger logger;
+        private StockHelper _stockhelper;
+
+        public DALDapper dbDapper
+        {
+            get
+            {
+                if (_dbDapper == null)
+                {
+                    _dbDapper = new DALDapper(ConnectionString);
+                }
+
+                return _dbDapper;
+            }
+        }
+
+
+        public string ConnectionString
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_ConnectionString))
+                {
+                    _ConnectionString = ConfigurationManager.ConnectionStrings[ConfigurationManager.AppSettings["DBDefault"]].ConnectionString;
+                }
+
+                return _ConnectionString;
+            }
+        }
+
+        public StockHelper Stockhelper
+        {
+            get
+            {
+                if (_stockhelper == null)
+                {
+                    _stockhelper = new StockHelper();
+                }
+                return _stockhelper;
+            }
+
+        }
+
+        public BaseForm()
+        {
+
+        }
+
+        public void InitForm()
+        {
+            //_dbDapper = new DALDapper(ConnectionString);
+            logger = NLog.LogManager.GetCurrentClassLogger();
+        }
+
+    }
+}
---
+++ M11Google/MainForm.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace M11Google
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(223, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(439, 392);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 416);
+            this.Controls.Add(this.richTextBox1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
